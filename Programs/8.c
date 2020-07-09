9d1f1d50e98f8bd758614e68ce0844ede929c531
Mohamed Arshad <mohamedarshadcholasseri5050@gmail.com>
2020-07-09 15:15:54 +0530

    8 Program Complet ,Take Output Screenshot

---
+++ Programs/8.c
@@ -0,0 +1,30 @@
+//Write a program to find the sum of all the odd numbers for a given limit
+
+
+#include <stdio.h>
+
+void main(){
+    
+   int limit,sum=0;
+
+   printf("Enter A Limit: \n");
+   scanf("%d",&limit);
+    sum=0;
+   for (int i = 0; i < limit; i++)
+   {
+       if (i%2!=0)
+       {
+           sum=sum+i;
+          
+       }
+      
+       
+       
+   }
+   printf("Sum Of Odd Number : %d\n",sum);
+   
+
+  
+
+ 
+}
