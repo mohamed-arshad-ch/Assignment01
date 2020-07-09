9a94589909e49b63457139927b72ebc2bc48ab9f
Mohamed Arshad <mohamedarshadcholasseri5050@gmail.com>
2020-07-09 14:15:39 +0530

    4 Program complete ,Take output screanshot

---
+++ Programs/4.c
@@ -0,0 +1,19 @@
+//Write a program to find the simple interest.
+#include <stdio.h>
+
+void main(){
+    
+    float mark;
+
+    printf("Enter Your Mark : \n");
+    scanf("%f",&mark);
+
+    if (mark>=50)
+    {
+        printf("Passed\n");
+    }else
+    {
+        printf("Failed\n");
+    }
+ 
+}
