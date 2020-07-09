c701bbd80a4a3bd8879b0fa67cdc494622f73a2d
Mohamed Arshad <mohamedarshadcholasseri5050@gmail.com>
2020-07-09 11:49:02 +0530

    First Program Completed Output ScreenShot Added

--- Programs/1.c
+++ Programs/1.c
@@ -1,5 +1,10 @@
+//1.Accept a char input from the user and display it on the console
 #include <stdio.h>
 
 void main(){
-    printf("HEllo\n");
+    char character;
+    printf("Enter A Character\n");
+    scanf("%c",&character);
+
+    printf("Enterd Charachter Is : %c\n",character);
 }
